038dab7912338c68ebeb8e3fec4ef4256cb379bb
Ramanenbh <124868539+Ramanenbh@users.noreply.github.com>
2023-06-08 12:44:04 +0800

    Add files via upload

---
+++ rwfb/src/components/Book/book_dropdown.js
@@ -0,0 +1,202 @@
+import React, { useState } from "react";
+import { BiChevronDown } from "react-icons/bi";
+import { AiOutlineSearch } from "react-icons/ai";
+import { UserAuthentication } from "../../LoginContext"
+import Navbar from "../../config/navbar";
+import { useNavigate } from "react-router-dom";
+
+export default function BookDropdown() {
+
+    const [setLocation, setSelectedLocation] = useState("");
+    const [setFacility, setSelectedFacility] = useState("");
+
+    //button click function 
+    const navigate = useNavigate();
+    const buttonClickHandle = async (e) => {
+        try {
+            e.preventDefault();
+            navigate('/book_calendar');
+        } catch (e) {
+            alert(e);
+            navigate('/signup');
+        }
+        
+    }; 
+
+    return (
+        <>
+        <Navbar/>
+        <div className="bg-red flex h-screen items-center justify-center">
+            <SelectLocation setLocation={setLocation} setSelectedLocation={setSelectedLocation}/>
+            <SelectFacility setFacility={setFacility} setSelectedFacility={setSelectedFacility} setLocation={setLocation}/>
+            <button class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            onClick={buttonClickHandle}>Check Venue Status</button>
+        </div>
+        </>
+    )
+}
+
+// dropdown to select the location eg: CAPT/TEMBU
+const SelectLocation = (props) => {
+
+    const [inputValue, setInputValue] = useState("");
+    const [open, setOpen] = useState(false);
+  
+    const { locations } = UserAuthentication();
+  
+    return (
+      <div className="bg-white flex h-screen items-center justify-center">
+          <div className="w-72 font-medium h-80">
+              
+          {/* Selecting the Location */}
+  
+          {/* setOpen - checks if its clicked or not */}
+          <div
+              onClick={() => setOpen(!open)}
+              className="bg-white w-full p-2 flex items-center justify-between rounded text-gray-700">
+              
+              {/* Shortens the Displayed result if its too long */}
+              {props.setLocation
+              ? props.setLocation?.length > 25
+                  ? props.setLocation?.substring(0, 25) + "..."
+                  : props.setLocation
+              : "Select Location"}
+  
+              {/* Selecting the Location's symbol */}
+              <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
+          </div>
+  
+          <ul
+              className={`bg-white mt-2 overflow-y-auto ${
+              open ? "max-h-60" : "max-h-0"
+              } `}
+          >
+  
+              {/* Search Bar */}
+              <div className="flex items-center px-2 sticky top-0 bg-white">
+              <AiOutlineSearch size={18} className="text-gray-700" />
+  
+              <input
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value.toUpperCase())}
+                  placeholder="Enter location name"
+                  className="placeholder:text-gray-700 p-2 outline-none"
+              />
+              </div>
+  
+              {/* Displaying the input */}
+              {locations?.map((data) => (
+              <li
+                  key={data?.Name}
+                  className={`p-2 text-sm hover:bg-sky-600 hover:text-white
+                  ${
+                  data?.Name?.toUpperCase() === props.setLocation?.toUpperCase() &&
+                  "bg-sky-600 text-white"
+                  }
+                  ${
+                  data?.Name?.toUpperCase().startsWith(inputValue)
+                      ? "block"
+                      : "hidden"
+                  }`}
+                  onClick={() => {
+                  if (data?.Name?.toUpperCase() !== props.setLocation.toUpperCase()) {
+                      props.setSelectedLocation(data?.Name);
+                      setOpen(false);
+                      setInputValue("");
+                  }
+                  }}
+              >
+                  {data?.Name}
+              </li>
+              ))}
+          </ul>
+          </div>
+      </div>
+    );
+  };
+
+  // dropdown to select the facility in that location
+  // select SR1 from selected location eg: CAPT
+  // facilities will be displayed based on location selected
+  const SelectFacility = (props) => {
+
+    const [inputValue, setInputValue] = useState("");
+    const [open, setOpen] = useState(false);
+  
+    const { facilities } = UserAuthentication();
+  
+    return (
+      <div className="bg-white flex h-screen items-center justify-center">
+          <div className="w-72 font-medium h-80">
+              
+          {/* Selecting the Facility */}
+  
+          {/* setOpen - checks if its clicked or not */}
+          <div
+              onClick={() => setOpen(!open)}
+              className="bg-white w-full p-2 flex items-center justify-between rounded text-gray-700">
+              
+              {/* Shortens the Displayed result if its too long */}
+              {props.setFacility
+              ? props.setFacility?.length > 25
+                  ? props.setFacility?.substring(0, 25) + "..."
+                  : props.setFacility
+              : "Select Location"}
+  
+              {/* Selecting the Facility's symbol */}
+              <BiChevronDown size={20} className={`${open && "rotate-180"}`} />
+          </div>
+  
+          <ul
+              className={`bg-white mt-2 overflow-y-auto ${
+              open ? "max-h-60" : "max-h-0"
+              } `}
+          >
+  
+              {/* Search Bar */}
+              <div className="flex items-center px-2 sticky top-0 bg-white">
+              <AiOutlineSearch size={18} className="text-gray-700" />
+  
+              <input
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value.toUpperCase())}
+                  placeholder="Enter Facility name"
+                  className="placeholder:text-gray-700 p-2 outline-none"
+              />
+              </div>
+  
+              {/* Displaying the input */}
+              {facilities?.filter(data => data.locationName === props.setLocation)
+              .map( (data) => (
+              <li
+                  key={data?.Name}
+                  className={`p-2 text-sm hover:bg-sky-600 hover:text-white
+                  ${
+                  data?.Name?.toUpperCase() === props.setFacility?.toUpperCase() &&
+                  "bg-sky-600 text-white"
+                  }
+                  ${
+                  data?.Name?.toUpperCase().startsWith(inputValue)
+                      ? "block"
+                      : "hidden"
+                  }`}
+                  onClick={() => {
+                  if (data?.Name?.toUpperCase() !== props.setLocation.toUpperCase()) {
+                      props.setSelectedFacility(data?.Name);
+                      setOpen(false);
+                      setInputValue("");
+                  }
+                  }}
+              >
+                  {data?.Name}
+              </li>
+              ))
+              }
+          </ul>
+          </div>
+      </div>
+    );
+  };
+
--- rwfb/src/components/Book/book_form.js
+++ rwfb/src/components/Book/book_form.js
@@ -1,1 +1,144 @@
+import Navbar from "../../config/navbar";
 
+const BookForm = () => {
+  return (
+    <div className='max-w-[600px] mx-auto my-16 p-4'>
+      <Navbar/>
+
+      <div class="flex items-center justify-center p-12">
+        
+    <form>
+    
+      {/* Name section */}
+      <div class="-mx-3 flex flex-wrap">
+
+        {/* First name */}
+        <div class="w-full px-3 sm:w-1/2">
+          <div class="mb-5">
+            <label
+              for="fName"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              First Name
+            </label>
+            <input
+              type="text"
+              name="fName"
+              id="fName"
+              placeholder="First Name"
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#B9261C] focus:shadow-md"
+            />
+          </div>
+        </div>
+
+        {/* Last name name */}
+        <div class="w-full px-3 sm:w-1/2">
+          <div class="mb-5">
+            <label
+              for="lName"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Last Name
+            </label>
+            <input
+              type="text"
+              name="lName"
+              id="lName"
+              placeholder="Last Name" required
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#B9261C] focus:shadow-md"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* Total Number of Pax using */}
+      <div class="mb-5">
+        <label
+          for="Pax"
+          class="mb-3 block text-base font-medium text-[#07074D]"
+        >
+          Total Number of Pax
+        </label>
+        <input
+          type="number"
+          name="Pax"
+          id="Pax"
+          min="0"
+          placeholder="20"
+          max="20" required
+          class="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#B9261C] focus:shadow-md"
+        />
+      </div>
+
+      {/* Date */}
+        <div class="w-full sm:w-1/2">
+          <div class="mb-5">
+            <label
+              for="date"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Date
+            </label>
+            <input
+              type="date"
+              name="date"
+              min="2023-05-05"
+              id="date" required
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#B9261C] focus:shadow-md"
+            />
+          </div>
+        </div>
+
+        {/* Time */}
+        <div class="-mx-3 flex flex-wrap">
+          <div class="w-full px-3 sm:w-1/2">
+            <label
+              for="time"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              Start Time
+            </label>
+            <input
+              type="time"
+              name="time"
+              step="1800"
+              id="time"
+              min="08:00" required
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#B9261C] focus:shadow-md"
+            />
+          </div>
+
+          <div class="w-full px-3 sm:w-1/2">
+            <label
+              for="time"
+              class="mb-3 block text-base font-medium text-[#07074D]"
+            >
+              End Time
+            </label>
+            <input
+              type="time"
+              name="time"
+              step="1800"
+              id="time"
+              min="08:00" required
+              class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#B9261C] focus:shadow-md"
+            />
+          </div>
+        </div>
+
+        {/* submit button */}
+        <div class="px-3 py-5 flex flex-col items-center">
+            <button
+            class="inline-flex items-center h-10 px-5 block text-base font-medium text-red-100 transition-colors duration-150 bg-red-600 rounded-lg focus:shadow-outline hover:bg-red-800"
+            >
+            Submit
+            </button>
+        </div>
+
+    </form>
+
+    </div>
+    </div>
+  )
+}
+export default BookForm;
