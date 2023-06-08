79ea57cdfe4c428904b8f9a5f8ac7bc0f8303ab3
Ramanenbh <124868539+Ramanenbh@users.noreply.github.com>
2023-06-08 12:44:44 +0800

    Add files via upload

--- rwfb/src/components/Home.js
+++ rwfb/src/components/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios'
-import { UserAuthentication } from '../LoginContext';
 
 import Navbar from "../config/navbar";
 import {ListDisplay, CalenderDisplay} from "../config/booking_display";
@@ -8,7 +7,6 @@ import {ListDisplay, CalenderDisplay} from "../config/booking_display";
 axios.defaults.withCredentials = true;
 
 const Home = () => {
-  const { user, logout } = UserAuthentication();
 
   return (
     <div className='max-w-[600px] mx-auto my-16 p-4'>
