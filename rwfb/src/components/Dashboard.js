6a6071f6db45e9da927f2e05929a8a1f4cfde105
jeronlxj <jeronliaw@u.nus.edu>
2023-07-12 16:37:50 +0800

    dashboard img

--- rwfb/src/components/Dashboard.js
+++ rwfb/src/components/Dashboard.js
@@ -14,18 +14,18 @@ const { user } = UserAuthentication();
     <div className='w-full h-[1200px] bg-center bg-cover bg-utown'>
     <div className='max-w-[800px] mx-auto my-16 p-4'>
       <Navbar name={user?.email} current={"Dashboard"}/>
-      <div className="mt-12">
+      <div className="mt-3">
         <div className="flex flex-wrap lg:flex-nowrap justify-center">
-            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-no-repeat bg-cover bg-center">
+            <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-erc bg-no-repeat bg-cover bg-center">
                 <div className="flex justify-between items-center">
                     <div>
                         <p className="font-bold text-gray-400">Total Bookings</p>
-                        <p className="text-2xl">2000,000</p>
+                        <p className="text-2xl">2100,000</p>
                     </div>
                 </div>
             </div>
             <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
-                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl">
+                <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-56 p-4 pt-9 rounded-2xl h-44">
                     <button type="button" style={{ color:'rgb(228, 106, 118)', backgroundColor: 'rgb(255, 244, 229)'}} className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"><MdOutlineSupervisorAccount/></button>
                     <p className="mt-3">
                         <span className="text-lg font-semibold">2965</span>
@@ -55,14 +55,10 @@ const { user } = UserAuthentication();
                             <span><GoPrimitiveDot/></span>
                             <span>Pending</span>
                         </p>
-                    </div>
-                    <div className="flex items-center gap-4">
                         <p className="flex items-center gap-2 text-pink-600 hover:drop-shadow-xl">
                             <span><GoPrimitiveDot/></span>
                             <span>Reviewed</span>
                         </p>
-                    </div>
-                    <div className="flex items-center gap-4">
                         <p className="flex items-center gap-2 text-green-600 hover:drop-shadow-xl">
                             <span><GoPrimitiveDot/></span>
                             <span>Approved</span>
@@ -92,22 +88,23 @@ const { user } = UserAuthentication();
                             </p>
                             <p className="text-gray-500 mt-1">Approved</p>
                         </div>
-                    </div>
+                        <div className="mt-5">
+                            <SparkLine currentColor="blue" id="line-sparkline" type="Line" height="80px" width="250px" data={[
+    { x: 1, y: 2 },
+    { x: 2, y: 6 },
+    { x: 3, y: 8 },
+    { x: 4, y: 5 },
+    { x: 5, y: 10 },
 
-                    <div className="mt-5">
-                        <SparkLine currentColor="blue" id="line-sparkline" type="Line" height="80px" width="250px" data={[
-  { x: 1, y: 2 },
-  { x: 2, y: 6 },
-  { x: 3, y: 8 },
-  { x: 4, y: 5 },
-  { x: 5, y: 10 },
-
-]} color="blue"/>
+    ]} color="blue"/>
+                        </div>
                     </div>
+                    <div>
+                        <Stacked width="320px" height="360px"/>
+                    </div>
+                    
                 </div>
-                <div>
-                    <Stacked width="320px" height="360px"/>
-                </div>
+                
             </div>
         </div>
       </div>
--- rwfb/tailwind.config.js
+++ rwfb/tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
       backgroundImage: (theme) => ({
         capt: "url('./capt-night-image.jpg')",
         utown: "url('./utown-night-image.jpg')",
+        erc: "url('./erc-wide.jpg')",
       }),
     },
   },
